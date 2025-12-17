9624334326bd25620ff3adc93c9624b0b304e48e
sanyam1805 <sanyam.1251090397@vit.edu>
2025-12-17 17:22:30 +0530

    feat: Add User model schema

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['student', 'staff', 'owner'],
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
